c9ba31ed80429dc2df7c60e3573061ae8428d997
denis.shtupa <denis.shtupa@3i-solutions.net>
2020-12-04 23:09:08 +0100

    contact section added

---
+++ src/app/my-profile/contact/profile-contact.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'profile-contact',
+    templateUrl: './profile-contact.component.html'
+})
+export class ProfileContactComponent implements OnInit {
+
+    model: any = {};
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+    }
+
+}
