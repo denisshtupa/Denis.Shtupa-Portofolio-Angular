9ad3acf8f7ed44f89d6fcdcff09f4175e0fd98bc
denis.shtupa <denis.shtupa@3i-solutions.net>
2020-12-15 18:31:17 +0100

    styling improvements, skills, intro and about components

--- src/app/my-profile/skills/profile-skills.component.ts
+++ src/app/my-profile/skills/profile-skills.component.ts
@@ -7,27 +7,27 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileSkillsComponent implements OnInit {
 
     public skillsListFE: ISkills[] = [
-        { id: 1, skill: "Angular v2-11", value: 95, percentage: "95%"},
-        { id: 2, skill: "Typescript", value: 90, percentage: "90%"},
-        { id: 3, skill: "HTML", value: 90, percentage: "90%"},
-        { id: 4, skill: "CSS", value: 90, percentage: "90%"},
-        { id: 5, skill: "Sass", value: 90, percentage: "90%"},        
-        { id: 6, skill: "Javascript", value: 85, percentage: "85%"},
+        { id: 1, skill: "Angular v2-11", value: 100, percentage: "100%"},
+        { id: 2, skill: "Typescript", value: 100, percentage: "100%"},
+        { id: 3, skill: "HTML", value: 100, percentage: "100%"},
+        { id: 4, skill: "CSS", value: 100, percentage: "100%"},
+        { id: 5, skill: "Sass", value: 95, percentage: "95%"},        
+        { id: 6, skill: "Javascript", value: 90, percentage: "90%"},
         { id: 7, skill: "jQuery", value: 85, percentage: "85%"},
         { id: 8, skill: "Vue.js", value: 75, percentage: "75%"},
         { id: 9, skill: "React.js", value: 70, percentage: "70%"}
     ];
 
     public skillsListBE: ISkills[] = [
-        { id: 1, skill: "SQL", value: 85, percentage: "85%"},
-        { id: 2, skill: "GIT", value: 85, percentage: "85%"},
-        { id: 3, skill: "C#", value: 80, percentage: "80%"},
-        { id: 4, skill: ".NET Core", value: 70, percentage: "70%"},
-        { id: 5, skill: "Entity Framework", value: 70, percentage: "70%"},
-        { id: 6, skill: "MVC5", value: 65, percentage: "65%"},
+        { id: 1, skill: "GIT", value: 100, percentage: "100%"},
+        { id: 2, skill: "SQL", value: 85, percentage: "85%"},
+        { id: 3, skill: "C#", value: 85, percentage: "85%"},
+        { id: 4, skill: ".NET Core", value: 85, percentage: "85%"},
+        { id: 5, skill: "Entity Framework", value: 80, percentage: "80%"},
+        { id: 6, skill: "MVC5", value: 70, percentage: "70%"},
         { id: 7, skill: "Amazon Web Services", value: 65, percentage: "65%"},
-        { id: 8, skill: "Docker", value: 60, percentage: "60%"},
-        { id: 9, skill: "Microsoft Azzure", value: 60, percentage: "60%"},
+        { id: 8, skill: "Docker", value: 65, percentage: "65%"},
+        { id: 9, skill: "Microsoft Azzure", value: 65, percentage: "65%"},
     ]
         
     
