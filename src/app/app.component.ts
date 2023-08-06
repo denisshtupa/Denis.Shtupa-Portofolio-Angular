4a030c837c87ef53748c2e298b4d29e714bcba3e
Denis Shtupa <denis.shtupa@gmail.com>
2023-08-06 23:58:14 +0200

    Auto-generated commit [ci skip]

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent {
-  title = 'Portfolio-Angular-11-Denis-Shtupa';
-}
--- src/app/my-profile/header/profile-header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'profile-header',
-  templateUrl: './profile-header.component.html'
-})
-export class ProfileHeaderComponent {
-  title = 'Portfolio-Angular-11-Denis-Shtupa';
-}
